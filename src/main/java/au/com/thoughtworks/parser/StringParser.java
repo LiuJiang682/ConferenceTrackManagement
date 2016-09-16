db437fdd3e1da19192167d7116169f2c43e4ee9c
Jiang Liu <liujiang682@gmail.com>
2016-09-16 10:59:08 +1000

    Added comment for StringParser

--- src/main/java/au/com/thoughtworks/parser/StringParser.java
+++ src/main/java/au/com/thoughtworks/parser/StringParser.java
@@ -9,6 +9,27 @@ public class StringParser implements Parser {
 	private static final int ONE = 1;
 	private static final int NOT_FOUND = -1;
 
+	/**
+	 * This method extracts the last word from a sentence. 
+	 * It will only return the last word of a standard sentence,
+	 * any other cases will NOT return any word! It will also
+	 * not return any word if the delim is null or empty.
+	 * For example:
+	 * 		 parserLastWord("last word", " ") => "word"
+	 *       parserLastWord(null, " ") => false
+	 *       parserLastWord("", " ") => false
+	 *       parserLastWord(" ", " ") => false
+	 *       parserLastWord("word", " ") => false
+	 *       parserLastWord("word", "d") => false
+	 *       parserLastWord("word", null) => false
+	 *       parserLastWord("word", "") => false
+	 *       
+	 * @param string the content string
+	 * @param delim the delim
+	 * 
+	 * @return last word of the content string or false 
+	 * 			of Optional
+	 */
 	@Override
 	public Optional<String> parseLastWord(String string, String delim) {
 		Optional<String> lastWord = Optional.empty(); 
